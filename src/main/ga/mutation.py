755496e39794d1156532602f05b76716777872a3
Miguel Angel Cabanillas Silva <miguelangelcabanillassilva@gmail.com>
2026-01-05 22:24:54 +0100

    Insertion mutation implemented

--- src/main/ga/mutation.py
+++ src/main/ga/mutation.py
@@ -2,10 +2,7 @@ import random
 from src.main.ga.chromosome import Chromosome
 
 
-def swap_mutation(
-    chromosome: Chromosome,
-    rng: random.Random
-) -> Chromosome:
+def swap_mutation(chromosome: Chromosome, rng: random.Random) -> Chromosome:
     """
     Swap mutation: swap two random positions of the chromosome.
     """
@@ -14,4 +11,17 @@ def swap_mutation(
     i, j = rng.sample(range(len(mutated)), 2)
     mutated[i], mutated[j] = mutated[j], mutated[i]
 
-    return mutated 
+    return mutated
+
+
+def insertion_mutation(chromosome: Chromosome, rng: random.Random) -> Chromosome:
+    """
+    Insertion mutation: remove one gene and insert it elsewhere.
+    """
+    mutated = chromosome.copy()
+
+    i, j = rng.sample(range(len(mutated)), 2)
+    gene = mutated.pop(i)
+    mutated.insert(j, gene)
+
+    return mutated
