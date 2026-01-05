2c08f0b757c416b5e5aedc3b7f89f581d567dba2
Miguel Angel Cabanillas <miguelangelcabanillas@MacBook-Air-de-Miguel.local>
2026-01-05 19:05:41 +0100

    Save readme with python execution command

---
+++ README.md
@@ -0,0 +1,5 @@
+To execute the code.
+
+Execute this command from the root folder
+
+python3.13 -m src.main.main
--- src/main/main.py
+++ src/main/main.py
@@ -2,8 +2,7 @@ from src.main.utils.jssp_instance import JSSPInstance, load_orlib_jobshop
 
 jssp_instance: JSSPInstance = load_orlib_jobshop("datasets/ft06.txt")
 
-print(jssp_instance.n_jobs)        # 6
-print(jssp_instance.n_machines)    # 6
-print(jssp_instance.total_operations)  # 36
-
+print(jssp_instance.n_jobs)
+print(jssp_instance.n_machines)
+print(jssp_instance.total_operations)
 print(jssp_instance.jobs)
